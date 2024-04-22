f47b13fda02311cec2990f2569e9979e436bde5c
Jekobukhrashvili <bukhrashvilijeko@gmail.com>
2024-04-22 23:59:08 +0400

    Add new files

---
+++ template-parts/header/site-header.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Displays the site header.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+$wrapper_classes  = 'site-header';
+$wrapper_classes .= has_custom_logo() ? ' has-logo' : '';
+$wrapper_classes .= ( true === get_theme_mod( 'display_title_and_tagline', true ) ) ? ' has-title-and-tagline' : '';
+$wrapper_classes .= has_nav_menu( 'primary' ) ? ' has-menu' : '';
+?>
+
+<header id="masthead" class="<?php echo esc_attr( $wrapper_classes ); ?>">
+
+<div>
+
+    <h1>
+        <a href="Skillwill.ge">J.B</a>
+    </h1>
+
+    <div>
+        <?php 
+        the_content();
+        ?>
+    </div>
+</div>
+
+</header><!-- #masthead -->
